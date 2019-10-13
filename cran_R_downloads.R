7ba04fcb98af79cf46d83b2b684df9a874719cb4
Alfredo G Marquez <alfredo.g.marquez@gmail.com>
2019-10-13 14:50:03 -0500

    get tsibble and gtrends

--- cran_R_downloads.R
+++ cran_R_downloads.R
@@ -4,23 +4,25 @@ library(cranlogs)
 library(lubridate)
 library(tidyverse)
 library(tsibble)
+library(gtrendsR)
 
 # Download R download data 
-cran_R_downloads_raw <- cranlogs::cran_downloads('R', from = "2015-01-01", to = lubridate::today())
+cran_R_downloads_raw <- cranlogs::cran_downloads('R', from = "2010-01-01", to = lubridate::today())
 cran_R_downloads <- cran_R_downloads_raw %>% 
   mutate(os_factor = forcats::fct_inorder(forcats::as_factor(os)),
          ver_factor =forcats::fct_inorder(forcats::as_factor(version)),
          year = lubridate::year(date),
          month = lubridate::month(date),
          week = lubridate::week(date),
-         yr_mo = stringr::str_c(year,'_',month),
-         yr_wk = stringr::str_c(year,'_',week)
-  )
+         yr_mo = tsibble::yearmonth(date),
+         yr_wk = tsibble::yearweek(date)
+  ) %>% 
+  filter(version != 'devel')
 
 # filter out devel
 
 # EDA
-cran_R_downloads %>% head() %>% View()
+cran_R_downloads %>% 
 
 cran_R_downloads %>% select(os) %>% group_by(os) %>% summarise(os_cnts = n())
 
@@ -36,25 +38,53 @@ down_tot_by_d <- cran_R_downloads %>%
   tsibble::as_tsibble()
 
 down_tot_by_d %>% ggplot(aes(x = yr_wk, y = total)) +
-  geom_line() +
-  scale_y_log10()
+  geom_line()
+
+down_tot_by_d %>% ggplot(aes(x = yr_wk, y = log10(total))) +
+  geom_line() 
 
 # What does the trend look like by os
 down_tot_by_os <- cran_R_downloads %>% 
-  dplyr:: group_by(date,os_factor) %>% 
+  dplyr:: group_by(yr_wk,os_factor) %>% 
   dplyr::summarise(total = sum(count))
 
-down_tot_by_os %>% ggplot(aes(x = date, y = total, col = os_factor, group = os_factor)) +
+down_tot_by_os %>% ggplot(aes(x = yr_wk, y = total, col = os_factor, group = os_factor)) +
   geom_line() +
-  facet_wrap(.~os_factor) +
-  scale_y_log10()
+  facet_wrap(.~os_factor) 
+
+down_tot_by_os %>% ggplot(aes(x = yr_wk, y = log10(total), col = os_factor, group = os_factor)) +
+  geom_line()
 
 # What does the trend look like by version
 down_tot_by_ver <- cran_R_downloads %>% 
-  dplyr:: group_by(date,ver_factor) %>% 
+  dplyr:: group_by(yr_wk,ver_factor) %>% 
   dplyr::summarise(total = sum(count))
 
-down_tot_by_ver %>% ggplot(aes(x = date, y = total, col = ver_factor, group = ver_factor)) +
+down_tot_by_ver %>% ggplot(aes(x = yr_wk, y = total, col = ver_factor, group = ver_factor)) +
   geom_line() +
-  facet_wrap(.~ver_factor) +
-  scale_y_log10()
+  facet_wrap(.~ver_factor)
+
+down_tot_by_ver %>% ggplot(aes(x = yr_wk, y = log10(total), col = ver_factor, group = ver_factor)) +
+  geom_line() +
+  facet_wrap(.~ver_factor)
+
+
+# Google Trends: Why is there a bump in the fall of 2018.
+
+R_gtrends <- gtrendsR::gtrends(c("R",'r'))
+
+r_trends_t <- R_gtrends$interest_over_time %>% 
+  dplyr::mutate(yr_wk = tsibble::yearweek(date),
+                hits_q = as.numeric(hits))
+
+r_trends_t %>% head()
+
+r_trends_tot_wk <- r_trends_t %>% 
+  dplyr::group_by(yr_wk) %>% 
+  dplyr::summarise(total = sum(hits_q)) 
+
+r_trends_tot_wk %>% ggplot(aes(x = yr_wk, y = total)) +
+  geom_line()
+
+r_trends_tot_wk %>% ggplot(aes(x = yr_wk, y = log10(total))) +
+  geom_line()
